cdbdb8311220f2ca68d25dfba3d2b62307564745
Kristofer Baxter <kristofer@kristoferbaxter.com>
2017-03-19 23:30:01 -0700

    About text rev 1.

--- src/about/views/about.js
+++ src/about/views/about.js
@@ -2,6 +2,11 @@ import {h} from 'preact';
 
 export default function AboutHome() {
   return (
-    <h1>About Home</h1>
+    <article>
+      <h1>Simple Hacker News Clone</h1>
+      <p>This is an example of a PWA built using Preact, and some opinionated tools.</p>
+      <p><strong>Please do not build an application this way</strong>. Instead view this as an example of some concepts used in modern web applications (sw, h2, h2push).</p>
+      <p>Made with kindness in California.</p>
+    </article>
   );
 }
