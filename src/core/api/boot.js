37381d5dbfab0be762ccd7ab612b06829417eb29
Kristofer Baxter <kristofer@kristoferbaxter.com>
2017-04-07 18:35:15 -0700

    Reduce first load JS by ~24% by fixing build process.

--- src/core/api/boot.js
+++ src/core/api/boot.js
@@ -1,18 +1,16 @@
 import {MemoryStore} from './memory.js';
 import {setLatestUUID} from './list.js';
 
-(() => {
-  if (window.seed !== undefined) {
-    const {uuid, items, $entities, max, type} = window.seed;
+if (window.seed !== undefined) {
+  const {uuid, items, $entities, max, type} = window.seed;
 
-    MemoryStore({
-      [uuid]: {
-        items,
-        max,
-        type
-      }
-    });
-    MemoryStore($entities);
-    setLatestUUID(type, uuid);
-  }
-})();
+  MemoryStore({
+    [uuid]: {
+      items,
+      max,
+      type
+    }
+  });
+  MemoryStore($entities);
+  setLatestUUID(type, uuid);
+}
