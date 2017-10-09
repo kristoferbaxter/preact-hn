241796ff9563cc9f630105e68d3cc17cc7206ec9
Kristofer Baxter <kristofer@kristoferbaxter.com>
2017-10-09 00:43:30 -0700

    Loading status for when pagination count is known but not any items for the page.

--- config/sw-precache.js
+++ config/sw-precache.js
@@ -1,22 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const jsFiles = fs.readdirSync(path.resolve('dist', 'chrome')).filter(filename => filename.endsWith('.js.br')).map(filename => `dist/chrome/${filename}`);
+const jsFiles = fs
+  .readdirSync(path.resolve('dist', 'chrome'))
+  .filter(filename => filename.endsWith('.js.br'))
+  .map(filename => `dist/chrome/${filename}`);
 
 module.exports = {
-  staticFileGlobs: ['/shell','dist/chrome/*.js'],
+  staticFileGlobs: ['/shell', 'dist/chrome/*.js'],
   dynamicUrlToDependencies: {
-    '/shell': jsFiles
+    '/shell': jsFiles,
   },
   navigateFallback: '/shell',
-  runtimeCaching: [{
-    urlPattern: /\/api\/list\//,
-    handler: 'networkFirst'
-  },{
-    urlPattern: /\/api\/comments\//,
-    handler: 'networkFirst'
-  },{
-    urlPattern: /\/api\/items\//,
-    handler: 'networkFirst'
-  }],
-}
+  runtimeCaching: [
+    {
+      urlPattern: /\/api\/list\//,
+      handler: 'networkFirst',
+    },
+    {
+      urlPattern: /\/api\/comments\//,
+      handler: 'networkFirst',
+    },
+    {
+      urlPattern: /\/api\/items\//,
+      handler: 'networkFirst',
+    },
+  ],
+};
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -5,21 +5,25 @@ import ListItem from 'components/ListItem';
 import {ITEMS_PER_PAGE} from 'utils/constants';
 import {PagedList} from 'api/api-types';
 
+import styles from './styles.css';
+
 interface ListViewProps {
   data: PagedList;
 }
-export default function({data}: ListViewProps): JSX.Element {
-  if (!data || data === null) {
+export default function({data = null}: ListViewProps): JSX.Element {
+  if (data === null) {
     return <Loading />;
   }
 
   const {items, $entities, max, page, type} = data;
+  const itemKeys = Object.keys(items);
   return (
-    <main>
+    <main class={styles.list}>
       <Pagination page={page} maxPages={Math.ceil(max / ITEMS_PER_PAGE)} type={type} />
-      {Object.keys(items).map(item => {
-        const itemAsInt = Number(item);
-        return <ListItem index={itemAsInt + 1} entity={$entities[items[itemAsInt]]} />;
+      {itemKeys.length === 0 && <Loading />}
+      {itemKeys.map(itemKey => {
+        const keyNumber = Number(itemKey);
+        return <ListItem index={keyNumber + 1} entity={$entities[items[keyNumber]]} />;
       })}
     </main>
   );
